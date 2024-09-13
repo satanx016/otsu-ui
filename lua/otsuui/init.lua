883d52965ee82449fa8d47f36def495d40844df5
Satanx016 <z.dadsi21@gmail.com>
2024-09-13 03:55:03 +0100

    feat: add custom otsu statuscolumn - fixes lsp signs overlapping with gitsings - added fold marks

--- lua/otsuui/init.lua
+++ lua/otsuui/init.lua
@@ -7,3 +7,6 @@ vim.opt.statusline = "%!v:lua.require('otsuui.otsuline." .. config.statusline.th
 if config.tabufline.enabled then
   require "otsuui.otsutab.lazyload"
 end
+
+-- init otsucolumn
+vim.opt.statuscolumn = [[%!v:lua.require'otsuui.otsucolumn'.statuscolumn()]]
