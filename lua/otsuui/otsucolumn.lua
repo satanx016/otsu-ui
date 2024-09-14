93d91382e597fa2c458cbaf240991a1177336966
Satanx016 <z.dadsi21@gmail.com>
2024-09-14 17:17:34 +0100

    style(diagnostic): disable warning for this line

--- lua/otsuui/otsucolumn.lua
+++ lua/otsuui/otsucolumn.lua
@@ -51,6 +51,7 @@ function M.foldtext()
   local ok = pcall(vim.treesitter.get_parser, vim.api.nvim_get_current_buf())
   local ret = ok and vim.treesitter.foldtext and vim.treesitter.foldtext()
   if not ret or type(ret) == "string" then
+---@diagnostic disable-next-line: cast-local-type
     ret = { { vim.api.nvim_buf_get_lines(0, vim.v.lnum - 1, vim.v.lnum, false)[1], {} } }
   end
   table.insert(ret, { " " .. '󰇘' })
