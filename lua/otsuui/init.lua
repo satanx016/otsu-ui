ee882922ebf9294075fa9acb140ddd9888e7f6f4
Satanx016 <z.dadsi21@gmail.com>
2024-09-19 20:56:15 +0100

    feat: adopt a normal plugin structure

--- lua/otsuui/init.lua
+++ lua/otsuui/init.lua
@@ -1,12 +1,38 @@
+local M = {}
+
 local config = require("nvconfig").ui
 
--- init otsuline
-vim.opt.statusline = "%!v:lua.require('otsuui.otsuline." .. config.statusline.theme .. "')()"
-
--- init otsutab
-if config.tabufline.enabled then
-  require "otsuui.otsutab.lazyload"
+M.setup = function()
+	M.otsuline()
+	M.otsutab()
+	M.otsucolumn()
 end
 
--- init otsucolumn
-vim.opt.statuscolumn = [[%!v:lua.require'otsuui.otsucolumn'.statuscolumn()]]
+M.otsuline = function()
+	vim.opt.statusline = "%!v:lua.require('otsuui.otsuline." .. config.statusline.theme .. "')()"
+end
+
+M.otsutab = function()
+	if config.tabufline.enabled then
+		require("otsuui.otsutab.lazyload")
+	end
+end
+
+M.otsucolumn = function()
+	vim.opt.statuscolumn = [[%!v:lua.require'otsuui.otsucolumn'.statuscolumn()]]
+
+	vim.opt.foldmethod = "expr"
+	vim.opt.foldtext = ""
+	vim.opt.foldlevel = 99
+	vim.opt.fillchars = {
+		foldopen = "",
+		foldclose = "",
+		fold = " ",
+		foldsep = " ",
+		diff = "╱",
+		eob = " ",
+	}
+	vim.opt.foldexpr = "v:lua.require'otsuui.otsucolumn'.foldexpr()"
+end
+
+return M
