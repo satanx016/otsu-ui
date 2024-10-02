d549398ed9086e2a78370ee4d30dda9e07a541c8
Satanx016 <z.dadsi21@gmail.com>
2024-10-02 16:04:02 +0100

    refactor(autocmds): migrate autocmds from `init.lua` to their proper file `autocmds.lua`

---
+++ lua/otsu-ui/autocmds.lua
@@ -0,0 +1,26 @@
+local function augroup(name)
+	return vim.api.nvim_create_augroup("otsuvim_" .. name, {})
+end
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+	group = augroup("reload"),
+	pattern = vim.uv.fs_realpath(vim.fn.stdpath("config") .. "/lua/otsuvim/config/otsurc.lua"),
+	callback = function()
+		-- reload otsurc
+		require("plenary.reload").reload_module("otsuvim.config.otsurc")
+		Otsuvim.config = require("otsuvim.config.otsurc")
+
+		require("plenary.reload").reload_module("based")
+		require("based").load_all_highlights()
+	end,
+})
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+	group = augroup("vsplit_help&man"),
+	pattern = { "*.txt", "*(*)" },
+	callback = function()
+		if vim.bo.filetype == "help" or vim.bo.filetype == "man" then
+			vim.cmd("wincmd L")
+		end
+	end,
+})
--- lua/otsu-ui/init.lua
+++ lua/otsu-ui/init.lua
@@ -5,31 +5,7 @@ function M.setup()
 	require("otsu-ui.otsuline").load()
 	require("otsu-ui.otsutab").lazyload()
 
-	M.reload_conf_on_save()
-	M.highlight_on_yank()
-end
-
-function M.highlight_on_yank()
-	vim.api.nvim_create_autocmd("TextYankPost", {
-		callback = function()
-			vim.highlight.on_yank({ higroup = "MatchWord" })
-		end,
-	})
-end
-
-function M.reload_conf_on_save()
-	vim.api.nvim_create_autocmd("BufWritePost", {
-		pattern = vim.uv.fs_realpath(vim.fn.stdpath("config") .. "/lua/otsuvim/config/otsurc.lua"),
-		group = vim.api.nvim_create_augroup("ReloadOtsu", {}),
-		callback = function()
-			-- reload otsurc
-			require("plenary.reload").reload_module("otsuvim.config.otsurc")
-			Otsuvim.config = require("otsuvim.config.otsurc")
-
-			require("plenary.reload").reload_module("based")
-			require("based").load_all_highlights()
-		end,
-	})
+	require("otsu-ui.autocmds")
 end
 
 return M
