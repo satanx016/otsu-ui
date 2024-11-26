96c3e2ae427776422d6f0b1677bbfe4b9308e2a8
Satanx016 <z.dadsi21@gmail.com>
2024-11-26 03:34:28 +0100

    chore: remove unused code

--- lua/otsu-ui/utils.lua
+++ lua/otsu-ui/utils.lua
@@ -29,22 +29,4 @@ M.replace_key_value = function(old, new)
   file:close()
 end
 
-M.set_cleanbuf_opts = function(ft)
-  local opt = vim.opt_local
-
-  opt.buflisted = false
-  opt.modifiable = false
-  opt.buftype = "nofile"
-  opt.number = false
-  opt.list = false
-  opt.wrap = false
-  opt.relativenumber = false
-  opt.cursorline = false
-  opt.colorcolumn = "0"
-  opt.foldcolumn = "0"
-
-  vim.opt_local.filetype = ft
-  vim.g[ft .. "_displayed"] = true
-end
-
 return M
