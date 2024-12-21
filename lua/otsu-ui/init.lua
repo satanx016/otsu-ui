3f8847f1bcaf0829e52d680403c76cf4eca1054d
Satanx016 <z.dadsi21@gmail.com>
2024-12-21 17:05:13 +0100

    fix: conditional loading

--- lua/otsu-ui/init.lua
+++ lua/otsu-ui/init.lua
@@ -1,9 +1,16 @@
 local M = {}
+local config = Otsuvim.config
 
 function M.setup()
+  if config.ui.statusline.enabled then
+    require("otsu-ui.statusline").load()
+  end
+
+  if config.ui.tufline.enabled then
+    require("otsu-ui.tufline").lazyload()
+  end
+
   require("otsu-ui.statuscolumn").load()
-  require("otsu-ui.statusline").load()
-  require("otsu-ui.tufline").lazyload()
 
   require("otsu-ui.autocmds")
 end
